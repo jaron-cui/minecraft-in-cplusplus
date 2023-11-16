fedcaa41941773c590d8a7f2fd464dfdf3611a83
jaron-cui <jaron.cui@gmail.com>
2023-11-16 18:27:13 -0500

    implemented collision physics

--- part1/include/EntityGod.hpp
+++ part1/include/EntityGod.hpp
@@ -2,8 +2,10 @@
 
 class EntityGod: public God {
   public:
+    EntityGod(World &world);
     void update() override;
     bool seesEntity(std::string name);
     void createEntity(Entity entity);
     void removeEntity(std::string name);
+    Entity getEntity(std::string name);
 };
